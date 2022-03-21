7029eb60f41a09218746290d05ffcd79bd7f2dfb
tzq0301 <1661610563@qq.com>
2022-03-21 15:32:41 +0800

    feat(index): 增加了 GitHub icon

--- src/components/index/MyIntroduction.vue
+++ src/components/index/MyIntroduction.vue
@@ -1,26 +1,40 @@
 <template>
-  <div id="container">
-    <div>
-      <img id="avatar" src="/public/images/tzq.jpg" alt="头像暂时无法显示">
+  <div class="container">
+    <div id="avatar-container">
+      <img id="avatar" src="public/images/tzq.jpg" alt="无法显示头像"/>
     </div>
+
+    <div style="height: 2vh"/>
+
     <div id="name">{{ name }}</div>
+
+    <div style="height: 1.2vh"/>
+
+    <div>
+      <MyIntroductionIcon
+          icon-path="public/icons/github.png"
+          icon-value="https://github.com/tzq0301"
+          text="tzq0301"/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import MyIntroductionIcon from "./MyIntroductionIcon.vue";
+
 const name = "tzq0301";
 </script>
 
 <style scoped lang="less">
 @import url(/src/css/fonts);
 
-#container {
+#avatar-container {
   display: flex;
-  flex-direction: column;
+  justify-content: center;
 }
 
 #avatar {
-  @avatar-length: 10vw;
+  @avatar-length: 12vw;
   width: @avatar-length;
   height: @avatar-length;
 
@@ -31,7 +45,7 @@ const name = "tzq0301";
 #name {
   text-align: center;
   font-family: merriweatherRegular, serif;
-  font-size: 16pt;
+  font-size: 18pt;
   color: white;
 }
 </style>
---
+++ src/components/index/MyIntroductionIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <img class="icon" :src="iconPath" alt="无法展示图标"/>
+    <span>&ensp;</span>
+    <span class="text" @click="locate">{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  iconPath: String,
+  iconValue: String,
+  text: String
+});
+
+const locate = () => {
+  window.open(props.iconValue || "")
+}
+</script>
+
+<style scoped lang="less">
+@import url(/src/css/fonts);
+
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+@icon-size: 1.2vw;
+.icon {
+  width: @icon-size;
+  height: @icon-size;
+}
+
+.text {
+  color: white;
+  font-family: merriweatherRegular, serif;
+  cursor: pointer;
+}
+</style>
