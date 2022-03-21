646f1455fb639e974a12e404d2dc44cc6c4ea26f
tzq0301 <1661610563@qq.com>
2022-03-21 20:55:32 +0800

    feat(index): 添加 Gmail

--- src/components/index/MyIntroduction.vue
+++ src/components/index/MyIntroduction.vue
@@ -4,17 +4,21 @@
       <img id="avatar" class="w-52 h-52 rounded-full object-cover" src="/public/images/tzq.jpg" alt="无法显示头像"/>
     </div>
 
-    <div class="h-6"/>
+    <div class="h-10"/>
 
     <div id="name" class="text-2xl text-white text-center mw-regular">{{ name }}</div>
 
-    <div class="h-3"/>
+    <div class="h-8"/>
 
-    <div>
+    <div class="flex justify-center items-center space-y-3 flex-col">
       <MyIntroductionIcon
           icon-path="/icons/github.png"
           icon-value="https://github.com/tzq0301"
-          text="tzq0301"/>
+          text="github.com/tzq0301"/>
+      <MyIntroductionIcon
+          icon-path="/icons/gmail.png"
+          icon-value="tzq0301@gmail.com"
+          text="tzq0301@gmail.com"/>
     </div>
   </div>
 </template>
--- src/components/index/MyIntroductionIcon.vue
+++ src/components/index/MyIntroductionIcon.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center flex-row">
     <img class="w-6 h-6" :src="iconPath" alt="无法展示图标"/>
     <span>&ensp;</span>
-    <span class="text-white text-center mw-regular pointer" @click="locate">{{ text }}</span>
+    <span class="text-white text-center mw-regular pointer text-sm" @click="locate">{{ text }}</span>
   </div>
 </template>
 
